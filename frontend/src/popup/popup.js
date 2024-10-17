d586e2dc9611551d1b9ee9fa0ae462c4adff74aa
mohamedBenma <mbenmaouche09@gmail.com>
2024-10-17 14:55:28 +0200

    update of how the pop up works

--- frontend/src/popup/popup.js
+++ frontend/src/popup/popup.js
@@ -1,30 +1,67 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const min = 0;
-    const max = 1;
-    const step = 0.1;
-    const slider = document.getElementById('myRange');
-    const ticksContainer = document.getElementById('ticks');
-    const numTicks = (max - min) / step;
-
-    for (let i = 0; i <= numTicks; i++) {
-        const tick = document.createElement('div');
-        ticksContainer.appendChild(tick);
-    }
-
-    let sliderValue = 0.5;
-    localStorage.setItem("sensitivity", sliderValue);
+    const toggleButton = document.getElementById("toggle-btn");
+    const toxicitySlider = document.getElementById("toxicity-slider");
+    const protectionStatus = document.querySelector(".protection-status");
+    const totalBlockedElement = document.querySelector(".total-blocked"); 
 
 
-    function sliderUpdate(value) {
-        sliderValue = value;
-        localStorage.setItem("sensitivity", sliderValue);
-    }
-
-    slider.addEventListener('input', function () {
-        sliderUpdate(this.value);
+    chrome.storage.sync.get("sensitivity", function (data) {
+        console.log(data)
+        const sensitivity = data.sensitivity || 0.5;
+        console.log(sensitivity)
+        toxicitySlider.value = sensitivity;
     });
 
-    slider.addEventListener('change', function () {
-        sliderUpdate(this.value);
+    chrome.storage.sync.get("protectionEnabled", function (data) {
+        const isEnabled = data.protectionEnabled || false;
+        toggleButton.checked = isEnabled;
+        updateProtectionStatus(isEnabled);
     });
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const currentTab = tabs[0];
+        const url = new URL(currentTab.url);
+        document.querySelector(".website").textContent = url.hostname;
+    });
+
+    toxicitySlider.addEventListener("input", function () {
+        const newValue = parseFloat(this.value);
+        chrome.storage.sync.set({ sensitivity: newValue }, function () {
+            console.log("Niveau de toxicité mis à jour :", newValue);
+        });
+
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { type: "updateSensitivity", sensitivity: newValue });
+        });
+    });
+
+    toggleButton.addEventListener("change", function () {
+        const isEnabled = this.checked;
+        chrome.storage.sync.set({ protectionEnabled: isEnabled }, function () {
+            console.log("État de la protection mis à jour :", isEnabled);
+            updateProtectionStatus(isEnabled);
+        });
+
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { type: "toggleProtection", enabled: isEnabled });
+        });
+    });
+
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        if (request.type === "updateTextLength") {
+            // Mettre à jour l'affichage de la longueur du texte
+            totalBlockedElement.textContent = `Longueur du Texte Traité : ${request.length}`;
+        }
+    });
+    function updateProtectionStatus(isEnabled) {
+        if (isEnabled) {
+            protectionStatus.textContent = "La protection est activée";
+            protectionStatus.classList.add("active");
+            protectionStatus.classList.remove("inactive");
+        } else {
+            protectionStatus.textContent = "La protection est désactivée";
+            protectionStatus.classList.add("inactive");
+            protectionStatus.classList.remove("active");
+        }
+    }
 });
--- frontend/src/textContent.js
+++ frontend/src/textContent.js
@@ -3,60 +3,84 @@ function delay(ms) {
 }
 
 const elementsToAnalyze = document.querySelectorAll('p, h1, h2, h3, div');
-
+let sensitivity = 0.5; 
+let protectionEnabled = false; 
 const rateLimit = 1;
+let totalTextLength=0;
 
 async function analyzeElements() {
-    chrome.storage.sync.get('sensitivity', async function (data) {
-        const sensitivity = data.sensitivity || 0.5;
+    totalTextLength=0;
+    if (!protectionEnabled) {
+        elementsToAnalyze.forEach(element => {
+            element.style.filter = 'none';
+            element.setAttribute('aria-hidden', 'false');
+        });
+        return;
+    }
 
-        for (let i = 0; i < elementsToAnalyze.length; i++) {
-            const element = elementsToAnalyze[i];
-            const elementText = element.innerText.trim();
+    for (let i = 0; i < elementsToAnalyze.length; i++) {
+        const element = elementsToAnalyze[i];
+        const elementText = element.innerText.trim();
+        totalTextLength += elementText.length;
+        console.log("Texte envoyé pour analyse : ", elementText);
 
-            console.log("Texte envoyé pour analyse : ", elementText);
+        try {
+            const response = await fetch('https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=AIzaSyAexXtPA33bp0hNSS3X_j30X3UhdCZ4u3I', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    comment: { text: elementText },
+                    languages: ['fr'],
+                    requestedAttributes: { TOXICITY: {} }
+                })
+            });
 
-            try {
-                const response = await fetch('https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=AIzaSyAexXtPA33bp0hNSS3X_j30X3UhdCZ4u3I', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        comment: { text: elementText },
-                        languages: ['fr'],
-                        requestedAttributes: { TOXICITY: {} }
-                    })
-                });
-
-                if (!response.ok) {
-                    throw new Error(`Erreur lors de la requête : ${response.statusText}`);
-                }
-
-                const data = await response.json();
-                const toxicityScore = data.attributeScores.TOXICITY.summaryScore.value;
-
-                console.log(`Score de TOXICITÉ pour l'élément "${elementText.substring(0, 50)}..." : ${toxicityScore}`);
-
-                if (toxicityScore > sensitivity) {
-                    console.log(`Floutage de l'élément suivant :`, element);
-                    element.style.filter = 'blur(5px)';
-                    element.setAttribute('aria-hidden', 'true');
-                } else {
-                    console.log(`Pas de floutage pour cet élément :`, element);
-                    element.style.filter = 'none';
-                    element.setAttribute('aria-hidden', 'false');
-                }
-            } catch (error) {
-                console.error('Erreur lors de l\'analyse du contenu :', error);
+            if (!response.ok) {
+                throw new Error(`Erreur lors de la requête :${response.statusText}`);
             }
 
-            if ((i + 1) % rateLimit === 0) {
-                await delay(1000);
+            const data = await response.json();
+            const toxicityScore = data.attributeScores.TOXICITY.summaryScore.value;
+            console.log('toxicity score: ',toxicityScore);
+            console.log(`Score de TOXICITÉ pour l'élément "${elementText.substring(0, 50)}..." : ${toxicityScore}`);
+
+            if (toxicityScore > sensitivity) {
+                console.log("flouté");
+                element.style.filter = 'blur(5px)';
+                element.setAttribute('aria-hidden', 'true');
+            } else {
+                console.log("non flouté");
+                element.style.filter = 'none';
+                element.setAttribute('aria-hidden', 'false');
             }
+        } catch (error) {
+            console.error('Erreur lors de l\'analyse du contenu :', error);
         }
-    });
+
+        if ((i + 1) % rateLimit === 0) {
+            await delay(1000);
+        }
+    }
+    console.log(totalTextLength);
+    chrome.runtime.sendMessage({ type: "updateTextLength", length: totalTextLength });
 }
 
 
-analyzeElements();
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === "updateSensitivity") {
+        sensitivity = message.sensitivity;
+        console.log("Sensibilité mise à jour :", sensitivity);
+        analyzeElements(); 
+    } else if (message.type === "toggleProtection") {
+        protectionEnabled = message.enabled;
+        analyzeElements(); 
+    }
+});
+
+chrome.storage.sync.get(["sensitivity", "protectionEnabled"], function (data) {
+    sensitivity = data.sensitivity || 0.5;
+    protectionEnabled = data.protectionEnabled || false;
+    analyzeElements();
+});
