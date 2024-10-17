6f3673393ad8c72e4c019d99b61e39e21cc04a07
Florian Dufly <107001405+Florian-Dufly@users.noreply.github.com>
2024-10-17 12:09:33 +0200

    front sending backend image as base64

--- frontend/src/imageContent.js
+++ frontend/src/imageContent.js
@@ -1,62 +1,46 @@
-const bucketName = "bucket-workshop-epsi-2024";
+const apiUrl = 'http://localhost:3000/upload-image';
+const sensitivity = 0.5;
 
-async function analyzeImage(imgElement, sensitivity) {
+async function analyzeImage(imgElement) {
     const imageUrl = imgElement.src;
-    console.log(imageUrl);
-    console.log(sensitivity);
 
+    const response = await fetch(imageUrl);
+    const blob = await response.blob();
+
+    const base64Image = await new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            resolve(reader.result);
+        }
+        reader.onerror = reject;
+        reader.readAsDataURL(blob);
+    });
+
+    console.log(base64Image);
 
     try {
-        const fileName = `temp-1.jpg`;
-        await uploadImageToBucket(imageUrl, fileName);
+        const formData = new FormData();
+        formData.append('imageBase64', base64Image);
+        console.log(formData);
 
-        const gcsUri = `gs://${bucketName}/${fileName}`;
-        const [result] = await visionClient.safeSearchDetection(gcsUri);
-        const detections = result.safeSearchAnnotation;
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            body: formData
+        });
 
-        console.log("Résultats de l'analyse de l'image :", detections);
-
-        const isSafe = analyzeImageDetections(detections, sensitivity);
-
-        if (!isSafe) {
-            blurElement(imgElement);
+        if (response.ok) {
+            const result = await response.json();
+            if (result.isOffensive) {
+                blurElement(imgElement);
+            } else {
+                unblurElement(imgElement);
+            }
         } else {
-            unblurElement(imgElement);
+            console.error('Erreur lors de l\'analyse de l\'image côté serveur');
         }
-
-        await deleteFileFromBucket(fileName);
     } catch (error) {
         console.error('Erreur lors de l\'analyse de l\'image :', error);
     }
-}
-
-async function uploadImageToBucket(imageUrl, fileName) {
-    const response = await fetch(imageUrl);
-    const buffer = await response.arrayBuffer();
-    const file = storage.bucket(bucketName).file(fileName);
-    await file.save(new Uint8Array(buffer));
-}
-
-function analyzeImageDetections(detections, sensitivity) {
-    const thresholdMap = {
-        'VERY_UNLIKELY': 0.1,
-        'UNLIKELY': 0.3,
-        'POSSIBLE': 0.5,
-        'LIKELY': 0.7,
-        'VERY_LIKELY': 0.9
-    };
-
-    const categories = ['adult', 'violence', 'racy'];
-    for (const category of categories) {
-        if (thresholdMap[detections[category]] > sensitivity) {
-            return false;
-        }
-    }
-    return true;
-}
-
-async function deleteFileFromBucket(fileName) {
-    await storage.bucket(bucketName).file(fileName).delete();
 }
 
 function blurElement(element) {
@@ -69,21 +53,12 @@ function unblurElement(element) {
     element.setAttribute('aria-hidden', 'false');
 }
 
-async function analyzeAllImagesOnPage(sensitivity) {
+async function analyzeAllImagesOnPage() {
     const imgElements = document.querySelectorAll('img');
 
     for (const imgElement of imgElements) {
-        const isSafe = await analyzeImage(imgElement, sensitivity);
-
-        if (!isSafe) {
-            blurElement(imgElement);
-        } else {
-            unblurElement(imgElement);
-        }
+        await analyzeImage(imgElement);
     }
 }
 
-
-//analyzeAllImagesOnPage(0.5);
-
-//analyzeImage("https://imgs.search.brave.com/ApbzVTxBtDqtxulYavQm-tlo5xsG0zpXb0PGGlOn5cM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cnVzdGljYS5mci9p/bWFnZXMvY2l0cm91/aWxsZS1lYjE1MDky/MS0wNDQtbDc5MC1o/NTI2LmpwZy53ZWJw", 0.5);
+analyzeAllImagesOnPage();
